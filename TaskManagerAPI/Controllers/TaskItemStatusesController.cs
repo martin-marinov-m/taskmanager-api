1afb19c83955788becdbafa7ec02bc2b06d20956
Martin Marinov <martinmarinov42@gmail.com>
2026-04-12 10:17:52 +0200

    Add TaskItemStatusesController with GetAll endpoint

---
+++ TaskManagerAPI/Controllers/TaskItemStatusesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models.Dtos;
+using TaskManagerAPI.Services;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemStatusesController : ControllerBase
+    {
+        private readonly ITaskItemStatusService _taskItemStatusService;
+
+        public TaskItemStatusesController(ITaskItemStatusService taskItemStatusService)
+        {
+            _taskItemStatusService = taskItemStatusService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItemStatusDto>>> GetAllStatusesAsync(CancellationToken ct)
+        {
+            var taskItemStatusDtos = await _taskItemStatusService.GetAllAsync(ct);
+
+            return Ok(taskItemStatusDtos);
+        }
+    }
+}
